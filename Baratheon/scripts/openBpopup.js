0cd664a5685547977091f8fdb3f74dfd1be6cc0a
momchilDishev <mo.dishev@gmail.com>
2014-12-08 14:43:56 -0700

    comments refactored and styled, songForm/Uploadform, new logo new styles.css

--- Baratheon/scripts/openBpopup.js
+++ Baratheon/scripts/openBpopup.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
     $(function() {
         // Binding a click event
         // From jQuery v.1.7.0 use .on() instead of .bind()
-        $('#my-button').bind('click', function(e) {
+        $('#upload-button').bind('click', function(e) {
             // Prevents the default action to be triggered. 
             e.preventDefault();
             // Triggering bPopup when click event is fired
